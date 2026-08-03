a2496d083dbef95cfab3c941a27f3ba6588b5df0
kotreshsn11-sudo <kotreshsn11@gmail.com>
2026-08-03 16:05:44 +0530

    commit

---
+++ src/main/java/com/ecommerce/backend/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package com.ecommerce.backend.controller;
+
+import com.ecommerce.backend.dto.CategoryResponse;
+import com.ecommerce.backend.service.CategoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+@CrossOrigin(originPatterns = "http://localhost:*")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Integer id) {
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+}
